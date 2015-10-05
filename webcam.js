9f9346735435abe554e33a734d1db2a49bd3894e
WoodMath <jeffwood314@gmail.com>
2015-10-05 16:03:57 -0500

    Added initial code for Web Cam

--- webcam.js
+++ webcam.js
@@ -6,18 +6,40 @@ var vbVertexBuffer, vbNormalBuffer;
 
 var objRender = new Geom();
 
+function testSetup(){
+/*
+	Code in 'testSetup()' function is based in part from tutorial at
+		http://www.html5rocks.com/en/tutorials/getusermedia/intro/
 
+*/
+	function hasGetUserMedia() {
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	}
 
+	if (hasGetUserMedia()) {
+		// Good to go!
+	} else {
+		alert('getUserMedia() is not supported in your browser');
+	}
+
+}
+
+function actualSetup(){
+	webcam_canvas_video_left = document.getElementById("webcam_canvas_video_left");
+	canvas = document.getElementById("webcam_canvas");		// this lets us know WHERE we are rendering
+	gl = canvas.getContext("experimental-webgl");			// this lets us know WHAT we are rendering (WebGL as o
+
+}
 
 function WebCamStart(){
 	
 	
 
 	
-	initGL();
-	initShaders();
-	initBuffers();
-	
+	testSetup();
+	actualSetup();	
+
 	draw();
 	
 	
